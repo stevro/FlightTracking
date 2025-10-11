d462a7ad8e43fee097ca128f1bd33564d97b559a
Stefan Matei <stev.matei@gmail.com>
2025-10-11 17:09:16 +0300

    fix

--- src/Provider/FlightRadar24/Model/FlightPosition.php
+++ src/Provider/FlightRadar24/Model/FlightPosition.php
@@ -122,7 +122,7 @@ class FlightPosition
     public $destIcao;
     /**
      * @var \DateTimeImmutable
-     * @JMS\Type("DateTimeImmutable<'Y-m-d\TH:i:s'>")
+     * @JMS\Type("DateTimeImmutable<'Y-m-d\TH:i:sZ'>")
      */
     public $eta;
 }
